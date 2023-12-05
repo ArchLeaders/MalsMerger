7198827193afdd6288d93df79c24a0b8b1467f92
ArchLeaders <archleadership28@gmail.com>
2023-12-05 15:49:54 -0800

    Stub Checksum ext

---
+++ src/MalsMerger/Core/Extensions/ChecksumExtension.cs
@@ -0,0 +1,9 @@
+﻿namespace MalsMerger.Core.Extensions;
+
+public static class ChecksumExtension
+{
+    public static bool IsVanilla(this byte[] buffer, string file, string archive)
+    {
+        return true;
+    }
+}
