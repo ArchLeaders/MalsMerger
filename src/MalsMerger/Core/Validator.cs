1fa19bb732db21624b9a398e604080e4da3f163d
ArchLeaders <archleadership28@gmail.com>
2023-12-05 13:56:21 -0800

    Impl Validator extension

---
+++ src/MalsMerger/Core/Validator.cs
@@ -0,0 +1,7 @@
+﻿namespace MalsMerger.Core;
+
+public static class Validator
+{
+    public static bool IsValidMalsFolder(ref string path)
+        => Path.GetFileName(path) == "Mals" || Directory.Exists(path = Path.Combine(path, "Mals"));
+}
