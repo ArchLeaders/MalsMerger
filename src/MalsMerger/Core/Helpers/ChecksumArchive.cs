b0bbd977e68882c500f67c6c6944564877751b51
ArchLeaders <archleadership28@gmail.com>
2023-12-05 15:52:26 -0800

    Impl ChecksumArchive

---
+++ src/MalsMerger/Core/Helpers/ChecksumArchive.cs
@@ -0,0 +1,37 @@
+﻿using Standart.Hash.xxHash;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace MalsMerger.Core.Helpers;
+
+public class ChecksumArchive
+{
+    private readonly Memory<byte> _buffer;
+
+    public ChecksumArchive(string version)
+    {
+        using Stream? stream = Assembly
+            .GetExecutingAssembly()
+            .GetManifestResourceStream(
+                $"MalsMerger.Resources.{version.Replace(".", string.Empty)}")
+                    ?? throw new NotSupportedException(
+                        $"The game version {version} is not supported");
+
+        byte[] buffer = new byte[stream.Length];
+        stream.Read(buffer);
+        _buffer = buffer;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public ulong Fetch(string key)
+        => Fetch(xxHash64.ComputeHash(key));
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public ulong Fetch(ulong key)
+    {
+        Span<ulong> cast = MemoryMarshal.Cast<byte, ulong>(_buffer.Span);
+        int half = cast.Length / 2;
+        return cast[half..][cast[..half].BinarySearch(key, Comparer<ulong>.Default)];
+    }
+}
