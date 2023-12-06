36b4e2175e9318c8ed07e2ac3a04ea3301a649cc
ArchLeaders <archleadership28@gmail.com>
2023-12-05 16:32:17 -0800

    Impl Checksum ext

--- src/MalsMerger.Core/Extensions/ChecksumExtension.cs
+++ src/MalsMerger.Core/Extensions/ChecksumExtension.cs
@@ -1,9 +1,37 @@
-﻿namespace MalsMerger.Core.Extensions;
+﻿using MalsMerger.Core.Models;
+using Standart.Hash.xxHash;
+
+namespace MalsMerger.Core.Extensions;
 
 public static class ChecksumExtension
 {
+    private static readonly Dictionary<string, ChecksumArchive> _archives = [];
+
     public static bool IsVanilla(this byte[] buffer, string file, string archive)
     {
-        return true;
+        string? version = null;
+        IEnumerable<string> segmented = archive.Split('.').Reverse();
+        foreach (string part in segmented) {
+            if (int.TryParse(part, out _)) {
+                version = part;
+                break;
+            }
+        }
+
+        if (version is null) {
+            throw new InvalidOperationException($"Could not parse version from file name: '{archive}'");
+        }
+
+        return xxHash64.ComputeHash(buffer) == GetArchive(version)
+            .Fetch(Path.Combine(archive, file).Replace('\\', '/'));
+    }
+
+    public static ChecksumArchive GetArchive(string version)
+    {
+        if (!_archives.TryGetValue(version, out ChecksumArchive? archive)) {
+            archive = new(version);
+        }
+
+        return archive;
     }
 }
