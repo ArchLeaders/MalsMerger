29d062f9f12478ba5ea1509598aa7dada0f37912
ArchLeaders <archleadership28@gmail.com>
2023-12-06 20:37:12 -0800

    Impl MsbtMerger

---
+++ src/MalsMerger.Core/Mergers/MsbtMerger.cs
@@ -0,0 +1,33 @@
+﻿using MalsMerger.Core.Extensions;
+using MessageStudio.Formats.BinaryText;
+
+namespace MalsMerger.Core.Mergers;
+
+public class MsbtMerger
+{
+    public static byte[] Merge(Span<byte> aBuffer, Span<byte> bBuffer, string msbtFile, string sarcFile)
+    {
+        Msbt msbtA = Msbt.FromBinary(aBuffer);
+        Msbt msbtB = Msbt.FromBinary(bBuffer);
+
+        foreach ((var label, var aEntry) in msbtA) {
+            if (!msbtB.ContainsKey(label)) {
+                Logger.WriteLine($"'{sarcFile}/{msbtFile}[{label}]' (source) was not found in the conflicting MSBT (Skipping...)", LogLevel.OK);
+                msbtB[label] = aEntry;
+                continue;
+            }
+
+            if (!aEntry.IsEntryVanilla(msbtFile, sarcFile, label)) {
+                Logger.WriteLine($"'{sarcFile}/{msbtFile}[{label}]' conflict found, using priority entry.", LogLevel.Info);
+                msbtB[label] = aEntry;
+                continue;
+            }
+
+            Logger.WriteLine($"'{sarcFile}/{msbtFile}[{label}]' (source) is a vanilla entry. (Skipping, conflict takes priority...)", LogLevel.OK);
+        }
+
+        using MemoryStream ms = new();
+        msbtB.ToBinary(ms, msbtB.Encoding, msbtB.Endian);
+        return ms.ToArray();
+    }
+}
