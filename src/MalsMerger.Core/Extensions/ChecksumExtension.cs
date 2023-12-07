5ebcdda1e40adfe6ee41449340a5da06c3c0dcc7
ArchLeaders <archleadership28@gmail.com>
2023-12-06 20:35:32 -0800

    Extend checksum extension (top level, nested, entry)

--- src/MalsMerger.Core/Extensions/ChecksumExtension.cs
+++ src/MalsMerger.Core/Extensions/ChecksumExtension.cs
@@ -1,4 +1,5 @@
 ﻿using MalsMerger.Core.Models;
+using MessageStudio.Formats.BinaryText;
 using Standart.Hash.xxHash;
 
 namespace MalsMerger.Core.Extensions;
@@ -7,10 +8,25 @@ public static class ChecksumExtension
 {
     private static readonly Dictionary<string, ChecksumArchive> _archives = [];
 
-    public static bool IsVanilla(this byte[] buffer, string file, string archive)
+    public static bool IsVanilla(this byte[] buffer, string sarcFile)
+    {
+        return IsVanillaCore(buffer, sarcFile, sarcFile.Replace('\\', '/'));
+    }
+
+    public static bool IsVanilla(this byte[] buffer, string msbtFile, string sarcFile)
+    {
+        return IsVanillaCore(buffer, sarcFile, Path.Combine(sarcFile, msbtFile).Replace('\\', '/'));
+    }
+
+    public static bool IsEntryVanilla(this MsbtEntry entry, string msbtFile, string sarcFile, string label)
+    {
+        return IsVanillaCore(System.Text.Encoding.UTF8.GetBytes(entry.Text + entry.Attribute ?? string.Empty), sarcFile, Path.Combine(sarcFile, msbtFile).Replace('\\', '/') + ':' + label);
+    }
+
+    private static bool IsVanillaCore(this byte[] buffer, string sarcFile, string key)
     {
         string? version = null;
-        IEnumerable<string> segmented = archive.Split('.').Reverse();
+        IEnumerable<string> segmented = sarcFile.Split('.').Reverse();
         foreach (string part in segmented) {
             if (int.TryParse(part, out _)) {
                 version = part;
@@ -19,11 +35,11 @@ public static class ChecksumExtension
         }
 
         if (version is null) {
-            throw new InvalidOperationException($"Could not parse version from file name: '{archive}'");
+            throw new InvalidOperationException($"Could not parse version from file name: '{sarcFile}'");
         }
 
         return xxHash64.ComputeHash(buffer) == GetArchive(version)
-            .Fetch(Path.Combine(archive, file).Replace('\\', '/'));
+            .Fetch(key);
     }
 
     public static ChecksumArchive GetArchive(string version)
