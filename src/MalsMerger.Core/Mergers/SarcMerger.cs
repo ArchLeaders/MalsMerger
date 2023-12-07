3c64a49daa6e4d646ce0bd4414a8bdcd6f3fab31
ArchLeaders <archleadership28@gmail.com>
2023-12-06 20:37:32 -0800

    Update SarcMerger impl & logging

--- src/MalsMerger.Core/Mergers/SarcMerger.cs
+++ src/MalsMerger.Core/Mergers/SarcMerger.cs
@@ -10,21 +10,31 @@ public class SarcMerger
         foreach ((var file, var aBuffer) in sarcA) {
             if (!sarcB.ContainsKey(file)) {
                 sarcB.Add(file, aBuffer);
+                Logger.WriteLine($"'{sarcFileName}/{file}' (source) was not found in the conflicting archive (Skipping...)", LogLevel.OK);
                 continue;
             }
 
             if (aBuffer.IsVanilla(file, sarcFileName)) {
-                // aBuffer is vanilla, so keep bBuffer as is
+                Logger.WriteLine($"'{sarcFileName}/{file}' (source) is a vanilla file. (Skipping, conflict takes priority...)", LogLevel.OK);
                 continue;
             }
 
             if (sarcB.TryGetValue(file, out byte[]? bBuffer)) {
                 if (bBuffer.IsVanilla(file, sarcFileName)) {
+                    Logger.WriteLine($"'{sarcFileName}/{file}' (conflict) is a vanilla file. (Skipping, source takes priority...)", LogLevel.OK);
                     sarcB[file] = aBuffer;
                 }
                 else {
-                    // both aBuffer and bBuffer are changed
-                    // attempt to merge the files
+                    if (aBuffer.AsSpan()[..8].SequenceEqual("MsgStdBn"u8) && bBuffer.AsSpan()[..8].SequenceEqual("MsgStdBn"u8)) {
+                        Logger.WriteLine($"Merging '{sarcFileName}/{file}'", LogLevel.Info);
+                        sarcB[file] = MsbtMerger.Merge(aBuffer, bBuffer, file, sarcFileName);
+                    }
+                    else {
+                        // If they aren't both MSBT files
+                        // aBuffer takes priority
+                        Logger.WriteLine($"Unknown file '{sarcFileName}/{file}', defaulting to priority file...", LogLevel.Warning);
+                        sarcB[file] = aBuffer;
+                    }
                 }
             }
         }
