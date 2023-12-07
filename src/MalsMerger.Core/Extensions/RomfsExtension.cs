6d01e5c9a39972fe9a9400839fb1dfbe9bea6d98
ArchLeaders <archleadership28@gmail.com>
2023-12-07 10:48:16 -0800

    Create RomFS extension (get version)

---
+++ src/MalsMerger.Core/Extensions/RomfsExtension.cs
@@ -0,0 +1,17 @@
+﻿namespace MalsMerger.Core.Extensions;
+
+public static class RomfsExtension
+{
+    public static int TryGetVersion(this string romfsFolder, int @default = 100)
+    {
+        string regionLangMask = Path.Combine(romfsFolder, "System", "RegionLangMask.txt");
+        if (File.Exists(regionLangMask)) {
+            string[] lines = File.ReadAllLines(regionLangMask);
+            if (lines.Length >= 3 && int.TryParse(lines[2], out int value)) {
+                return value;
+            }
+        }
+
+        return @default;
+    }
+}
