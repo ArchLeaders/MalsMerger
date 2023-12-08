2270cde455c4b51424b8ed6385a0050e43470823
ArchLeaders <archleadership28@gmail.com>
2023-12-07 21:17:38 -0800

    Use global checksums

--- src/MalsMerger.Core/Extensions/ChecksumExtension.cs
+++ src/MalsMerger.Core/Extensions/ChecksumExtension.cs
@@ -1,21 +1,21 @@
-﻿using MalsMerger.Core.Models;
-using MessageStudio.Formats.BinaryText;
-using Standart.Hash.xxHash;
+﻿using Standart.Hash.xxHash;
+using System.Runtime.InteropServices;
 
 namespace MalsMerger.Core.Extensions;
 
 public static class ChecksumExtension
 {
-    private static readonly Dictionary<string, ChecksumArchive> _archives = [];
+    private static readonly string _path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Totk", "checksums.bin");
+    private static readonly byte[] _buffer = File.ReadAllBytes(_path);
 
     public static bool IsVanilla(this byte[] buffer, string sarcFile)
     {
-        return IsVanillaCore(buffer, sarcFile, sarcFile.Replace('\\', '/'));
+        return IsVanillaCore(buffer, sarcFile, Path.Combine("Mals", sarcFile).Replace('\\', '/'));
     }
 
     public static bool IsVanilla(this byte[] buffer, string msbtFile, string sarcFile)
     {
-        return IsVanillaCore(buffer, sarcFile, Path.Combine(sarcFile, msbtFile).Replace('\\', '/'));
+        return IsVanillaCore(buffer, sarcFile, Path.Combine("Mals", sarcFile, msbtFile).Replace('\\', '/'));
     }
 
     private static bool IsVanillaCore(this byte[] buffer, string sarcFile, string key)
@@ -33,16 +33,22 @@ public static class ChecksumExtension
             throw new InvalidOperationException($"Could not parse version from file name: '{sarcFile}'");
         }
 
-        return xxHash64.ComputeHash(buffer) == GetArchive(version)
-            .Fetch(key);
+        return xxHash64.ComputeHash(buffer) == GetChecksum(key, version);
     }
 
-    public static ChecksumArchive GetArchive(string version)
+    public static ulong GetChecksum(string key, string version)
     {
-        if (!_archives.TryGetValue(version, out ChecksumArchive? archive)) {
-            archive = new(version);
+        Span<ulong> cast = MemoryMarshal.Cast<byte, ulong>(_buffer);
+        int half = cast.Length / 2;
+
+        int index;
+        if ((index = cast[..half].BinarySearch(xxHash64.ComputeHash($"{key}#{version}"))) > -1) {
+            return cast[half..][index];
+        }
+        else if ((index = cast[..half].BinarySearch(xxHash64.ComputeHash(key))) > -1) {
+            return cast[half..][index];
         }
 
-        return archive;
+        return ulong.MinValue;
     }
 }
